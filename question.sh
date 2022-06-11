1fb4b09d2fc9b3e5f3373abfe18f6e59f7220af5
Diana Lucía Medina Nieto <95106092+dlmedinan08@users.noreply.github.com>
2022-06-11 18:13:51 -0500

    Final Solution

--- question.sh
+++ question.sh
@@ -51,6 +51,6 @@ sed -i 's/-\([0-9]\)\-/-0\1-/' preprocess1.csv
 sed -i 's/-\([0-9][0-9]\)\,/-20\1,/' preprocess1.csv
 sed -i 's/^\([0-9]\)\-/0\1-/' preprocess1.csv
 sed -i 's/,$/,\\\N/' preprocess1.csv
-sed -i 's/\([0-9][0-9]\)-\([0-9][0-9]\)-\([0-9]*\),/\3-\2-\1,/' preprocess1.csv
+sed 's/\([0-9][0-9]\)-\([0-9][0-9]\)-\([0-9]*\),/\3-\2-\1,/' preprocess1.csv > output.csv
 
-cat preprocess1.csv
+cat output.csv
