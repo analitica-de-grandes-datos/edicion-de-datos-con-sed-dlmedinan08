08d194a35f43b9c81ee6590a4c43488c134934af
Diana Lucía Medina Nieto <95106092+dlmedinan08@users.noreply.github.com>
2022-06-25 17:56:16 -0500

    Progress_11

--- question.sh
+++ question.sh
@@ -41,16 +41,15 @@
 #
 #  >>> Escriba su codigo a partir de este punto <<<
 #
-cat data.csv > preprocess1.csv
-sed -i 's/,/./g' preprocess1.csv
-sed -i 's/;/,/g' preprocess1.csv
-sed -i 's/[a-z]/\U&/g' preprocess1.csv
-sed -i 's/,N/,\\N/g' preprocess1.csv
-sed -i 's/,,/,\\N,/g' preprocess1.csv
-sed -i 's/\//-/g' preprocess1.csv
-sed -i 's/-\([0-9]\)\-/-0\1-/' preprocess1.csv
-sed -i 's/-\([0-9][0-9]\)\,/-20\1,/' preprocess1.csv
-sed -i 's/^\([0-9]\)\-/0\1-/' preprocess1.csv
-sed -i 's/,$/,\\\N/' preprocess1.csv
-sed -i 's/\([0-9][0-9]\)-\([0-9][0-9]\)-\([0-9]*\),/\3-\2-\1,/' preprocess1.csv
-cat preprocess1.csv > output.csv
+cat data.csv \
+| sed 's/,/./g' \
+| sed 's/;/,/g' \
+| sed 's/[a-z]/\U&/g' \
+| sed 's/,N/,\\N/g' \
+| sed 's/,,/,\\N,/g' \
+| sed 's/\//-/g' \
+| sed 's/-\([0-9]\)\-/-0\1-/' \
+| sed 's/-\([0-9][0-9]\)\,/-20\1,/' \
+| sed 's/^\([0-9]\)\-/0\1-/' \
+| sed 's/,$/,\\\N/' \
+| sed 's/\([0-9][0-9]\)-\([0-9][0-9]\)-\([0-9]*\),/\3-\2-\1,/'
